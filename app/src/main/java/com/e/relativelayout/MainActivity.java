b0bcf515a9036c833cd8decbccd6233191c4ed9c
loots <ludotracks@hotmail.com>
2019-12-04 18:42:45 +0100

    final

---
+++ app/src/main/java/com/e/relativelayout/MainActivity.java
@@ -0,0 +1,18 @@
+package com.e.relativelayout;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.drawable.Drawable;
+import android.os.Bundle;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+}
